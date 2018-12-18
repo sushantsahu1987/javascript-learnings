5a3b73b177fdbaaff238fe674c31d6fdfc47ebf6
Sushant Sahu <sushant87@gmail.com>
2018-12-18 20:05:57 +0530

    slice vs splice

---
+++ jsbasics/slicesplice.js
@@ -0,0 +1,14 @@
+// splice changes orignal array
+
+let array1 = [1,3,5,6,7,8]
+
+console.log('splice');
+console.log(array1.splice(2));
+console.log(array1);
+
+// splice return new changed array
+let array2 = [1,3,5,6,7,8]
+
+console.log('slice');
+console.log(array2.slice(2));
+console.log(array2);
