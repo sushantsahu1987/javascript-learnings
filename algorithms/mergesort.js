bffde162564c86b4b0927d7c9c429de28315200a
Sushant Sahu <sushant87@gmail.com>
2018-12-18 19:52:56 +0530

    merge sort uodate

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,42 +1,43 @@
 // Split the array into halves and merge them recursively 
-function mergeSort (arr) {
-    if (arr.length === 1) {
-      // return once we hit an array with a single item
-      return arr
-    }
-  
-    const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
-    const left = arr.slice(0, middle) // items on the left side
-    const right = arr.slice(middle) // items on the right side
-  
-    return merge(
-      mergeSort(left),
-      mergeSort(right)
-    )
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    // return once we hit an array with a single item
+    return arr
   }
-  
-  // compare the arrays item by item and return the concatenated result
-  function merge (left, right) {
-    let result = []
-    let indexLeft = 0
-    let indexRight = 0
-  
-    while (indexLeft < left.length && indexRight < right.length) {
-      if (left[indexLeft] < right[indexRight]) {
-        result.push(left[indexLeft])
-        indexLeft++
-      } else {
-        result.push(right[indexRight])
-        indexRight++
-      }
+
+  const middle = Math.floor(arr.length / 2) // get the middle item of the array rounded down
+  const left = arr.slice(0, middle) // items on the left side
+  const right = arr.slice(middle) // items on the right side
+
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+// compare the arrays item by item and return the concatenated result
+function merge(left, right) {
+  let result = []
+  let indexLeft = 0
+  let indexRight = 0
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft])
+      indexLeft++
+    } else {
+      result.push(right[indexRight])
+      indexRight++
     }
-  
-    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
   }
-  
+
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight))
+}
+
 // let count  = alternating();
 
-let arr = [5, 1, 2, 4, 0];
+// let arr = [5, 1, 2, 4, 0];
+let arr = ["ab","ac","x", "e", "f", "b", "d"];
 console.log(arr);
 let result = mergeSort(arr);
 console.log(result);
